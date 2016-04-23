eb5b8a6a4c4487403a608fa2278cc73dc2e57cfe
nieatnh <nieatnh@gmail.com>
2016-04-23 17:39:33 +0000

    Added scale helper class

---
+++ Assets/CelestialScale.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CelestialScale {
+    //https://www.nasa.gov/pdf/180561main_ETM.Distance.Moon.pdf
+    public const double MoonRadiusKm = 3475 / 2;
+    public const double MoonAverageDistance = 382500;
+
+    //http://www.nasa.gov/mission_pages/voyager/multimedia/pia17046.html#.VxuVMTB974Z
+    public const double SunRadiusKm = 695508;
+    public const double SunAproxDistance = 150000000;
+
+    public struct ScaledInfo
+    {
+        public double AproxDistance;
+        public double Radius;
+    }
+
+    public static ScaledInfo MoonScale(double targetRadius) 
+    {
+        ScaledInfo result = new ScaledInfo();
+        result.Radius = targetRadius;
+        result.AproxDistance = MoonAverageDistance / MoonRadiusKm * targetRadius;
+        return result;
+    }
+
+    public static ScaledInfo SunScale(double targetRadius)
+    {
+        ScaledInfo result = new ScaledInfo();
+        result.Radius = targetRadius;
+        result.AproxDistance = SunAproxDistance / SunRadiusKm * targetRadius;
+        return result;
+    }
+}
